c195f0547a061a55404245cb8af7c0583322e600
Tuubaas <tobli@student.chalmers.se>
2019-02-19 16:36:49 +0100

    Implemented initial components

---
+++ recipe-app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+
+      <div>
+        <h2>MERN-stack recipe app</h2>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
