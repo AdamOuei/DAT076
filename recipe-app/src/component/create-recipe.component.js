45f9a7d46b85fb8757350c0b730344582cfd310b
Kitzing <sara.kitzing@hotmail.com>
2019-02-20 15:07:54 +0100

    Added dropdown with checkboxes for create recipe

--- recipe-app/src/component/create-recipe.component.js
+++ recipe-app/src/component/create-recipe.component.js
@@ -1,32 +1,53 @@
 import React, {Component} from 'react';
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
+
+const options = [
+  { label: 'Kyckling', value: 1},
+  { label: 'Vegetariskt', value: 2},
+  { label: 'Grekiskt', value: 3},
+  { label: 'Fisk', value: 4},
+  { label: 'Asiatiskt', value: 5},
+];
 
 export default class CreateRecipe extends Component {
 
-    
       render() {
         return (
             <div>
                 <p>Create Recipe</p>
-            
-          <form>
-            <label>
-              Title: 
-              <input type="text" />
-            </label>
-            <br/>
-            <label>
-                Ingredients: 
-                <textarea></textarea>
-            </label>
-            <br/>
-            <label>
-                Instructions: 
-                <textarea></textarea>
-            </label>
-            <br/>
-            <input type="submit" value="Submit" />
-          </form>
-          </div>
+                <div className="container">
+                  <div className="row mt-5">
+                    <div className="col-sm-12">
+                      <form>
+                        <label>
+                          Title: 
+                          <input type="text" />
+                        </label>
+                        <br/>
+                        <label>
+                            Ingredients: 
+                            <textarea></textarea>
+                        </label>
+                        <br/>
+
+                        <label>
+                            Instructions: 
+                            <textarea></textarea>
+                        </label>
+                        <br/>
+
+                        <label>
+                          Category: 
+                          <ReactMultiSelectCheckboxes options={options} />
+                      
+                        </label>
+                        <br/>
+                        <input type="submit" value="Submit" />
+                    </form>
+                  </div>
+                  </div>
+                </div>
+            </div>
         );
       }
     }
