4cdefcb9b7b7be6e2df01d56550dcd250268d89d
Tuubaas <tobli@student.chalmers.se>
2019-02-20 09:36:16 +0100

    added navbar with links

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -15,11 +15,27 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          <h2>MERN-stack recipe app</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/" target="_blank">
+              <img src={logo} width="30" height="30" alt="RecipeList" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={RecipeList} />
+          <Route path="/edit/:id" component={EditRecipe} />
+          <Route path="/create" component={CreateRecipe} />
         </div>
-        <Route path="/" exact component={RecipeList} />
-        <Route path="/edit/:id" component={EditRecipe} />
-        <Route path="/create" component={CreateRecipe} /> 
       </Router>
     );
   }
