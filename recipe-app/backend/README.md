278542d2587469ef4a366f1ef02a50707bb38e62
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2019-02-20 12:55:52 +0100

    Initial connection to database added

---
+++ recipe-app/backend/README.md
@@ -0,0 +1,9 @@
+# npm install
+
+# Start the database
+npm start-db (from backend folder).
+
+# How to start the server
+npm start
+You need to have the database running in order for the server to connect to it
+
--- recipe-app/backend/server.js
+++ recipe-app/backend/server.js
@@ -3,10 +3,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.listen(PORT, function() {
+mongoose.connect('mongodb://127.0.0.1:27017/recipe-app', {
+    useNewUrlParser: true
+});
+const connection = mongoose.connection;
+
+connection.once('open', function () {
+    console.log("MongoDB database connection established succesfully");
+})
+
+app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
 });
