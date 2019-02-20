95c93f66397b6ea4e68608fce95100b3f7ed2739
Kitzing <sara.kitzing@hotmail.com>
2019-02-20 13:17:22 +0100

    Added fields in create recipe

--- recipe-app/src/component/create-recipe.component.js
+++ recipe-app/src/component/create-recipe.component.js
@@ -1,11 +1,43 @@
 import React, {Component} from 'react';
 
 export default class CreateRecipe extends Component {
-    render() {
+
+    
+      render() {
+        return (
+            <div>
+                <p>Create Recipe</p>
+            
+          <form>
+            <label>
+              Title: 
+              <input type="text" />
+            </label>
+            <br/>
+            <label>
+                Ingredients: 
+                <textarea></textarea>
+            </label>
+            <br/>
+            <label>
+                Instructions: 
+                <textarea></textarea>
+            </label>
+            <br/>
+            <input type="submit" value="Submit" />
+          </form>
+          </div>
+        );
+      }
+    }
+
+ /*   render() {
         return (
             <div>
                 <p>CreateRecipe</p>
             </div>
         )
-    }
+    } 
+   
 }
+ */
