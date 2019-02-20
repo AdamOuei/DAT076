373e252db2cea09f39b13226568f78ebf1d623fd
Kitzing <sara.kitzing@hotmail.com>
2019-02-20 16:31:03 +0100

    Added miniature recipe component

---
+++ recipe-app/src/component/recipe-miniature.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class MiniRecipe extends Component {
+
+    render(){
+        return(
+            <div>
+            <p>Show MiniRecipe</p>
+        </div>
+        );
+    }
+
+}
