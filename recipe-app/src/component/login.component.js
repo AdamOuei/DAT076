a85c88ab56b824216eb22cd11313849086cb247d
Omar Oueidat <oueidat@student.chalmers.se>
2019-02-21 13:55:09 +0100

    Create a form for loginpage

--- recipe-app/src/component/login.component.js
+++ recipe-app/src/component/login.component.js
@@ -1,18 +1,67 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import Register from "./register-user.component";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <Router>
-        <div>
-          <p>Login thingy here</p>
-          <Link to="/register" className="nav-link">
-            Register if not already
-          </Link>
+        <div className="Login">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="email" bSize="large">
+              <FormLabel>Email</FormLabel>
+              <FormControl
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup controlId="password" bSize="large">
+              <FormLabel>Password</FormLabel>
+              <FormControl
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <Button
+              block
+              bSize="large"
+              disabled={!this.validateForm()}
+              type="submit"
+            >
+              Login
+            </Button>
+            <Link to="/register" className="nav-link">
+              Register if not already
+            </Link>
 
-          <Route path="/register" component={Register} />
+            <Route path="/register" component={Register} />
+          </form>
         </div>
       </Router>
     );
