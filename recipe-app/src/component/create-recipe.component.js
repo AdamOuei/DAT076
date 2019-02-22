eae542612bd3bd4c0b3fc19f8ec38a8640dcd78b
Omar Oueidat <oueidat@student.chalmers.se>
2019-02-22 13:36:10 +0100

    Add states for create recipe and handleChanges

--- recipe-app/src/component/create-recipe.component.js
+++ recipe-app/src/component/create-recipe.component.js
@@ -1,58 +1,82 @@
-import React, {Component} from 'react';
-import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
+import React, { Component } from "react";
+import ReactMultiSelectCheckboxes from "react-multiselect-checkboxes";
 
 const options = [
-  { label: 'Kyckling', value: 1},
-  { label: 'Vegetariskt', value: 2},
-  { label: 'Grekiskt', value: 3},
-  { label: 'Fisk', value: 4},
-  { label: 'Asiatiskt', value: 5},
+  { label: "Kyckling", value: 1 },
+  { label: "Vegetariskt", value: 2 },
+  { label: "Grekiskt", value: 3 },
+  { label: "Fisk", value: 4 },
+  { label: "Asiatiskt", value: 5 }
 ];
 
 export default class CreateRecipe extends Component {
+  constructor(props) {
+    super(props);
 
-      render() {
-        return (
-            <div>
-                <p>Create Recipe</p>
-                <div className="container">
-                  <div className="row mt-5">
-                    <div className="col-sm-12">
-                      <form>
-                        <label>
-                          Title: 
-                          <input type="text" />
-                        </label>
-                        <br/>
-                        <label>
-                            Ingredients: 
-                            <textarea></textarea>
-                        </label>
-                        <br/>
+    this.state = {
+      title: "",
+      ingredients: [],
+      instructions: [],
+      categories: []
+    };
 
-                        <label>
-                            Instructions: 
-                            <textarea></textarea>
-                        </label>
-                        <br/>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-                        <label>
-                          Category: 
-                          <ReactMultiSelectCheckboxes options={options} />
-                      
-                        </label>
-                        <br/>
-                        <input type="submit" value="Submit" />
-                    </form>
-                  </div>
-                  </div>
-                </div>
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      /*TODO: Make into a form so that we can setStates above*/
+
+      <div>
+        <p>Create Recipe</p>
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-sm-12">
+              <form>
+                <label>
+                  Title:
+                  <input type="text" />
+                </label>
+                <br />
+                <label>
+                  Ingredients:
+                  <textarea />
+                </label>
+                <br />
+
+                <label>
+                  Instructions:
+                  <textarea />
+                </label>
+                <br />
+
+                <label>
+                  Category:
+                  <ReactMultiSelectCheckboxes options={options} />
+                </label>
+                <br />
+                <input type="submit" value="Submit" />
+              </form>
             </div>
-        );
-      }
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
- /*   render() {
+/*   render() {
         return (
             <div>
                 <p>CreateRecipe</p>
