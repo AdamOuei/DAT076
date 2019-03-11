0d911b6f162f83d2d72333b70fd1afa0e596d5d5
Tuubaas <tobli@student.chalmers.se>
2019-03-11 16:12:06 +0100

    Fixed login

--- recipe-app/src/component/login.component.js
+++ recipe-app/src/component/login.component.js
@@ -9,23 +9,13 @@ export default class Login extends Component {
     constructor(props) {
         super(props);
 
-
-    this.state = {
-      email: "",
-      password: "",
-      loggedIn: false,
-      msg: ""
-    };
-  }
-
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  };
+        this.state = {
+            email: "",
+            password: "",
+            loggedIn: false,
+            msg: ""
+        };
+    }
 
     validateForm() {
         return this.state.email.length > 0 && this.state.password.length > 0;
@@ -36,6 +26,14 @@ export default class Login extends Component {
         });
     };
 
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    };
 
     validateLogin(input) {
         this.setState({
@@ -43,69 +41,77 @@ export default class Login extends Component {
         });
     }
 
-  handleSubmit = async event => {
-    console.log(this.context);
-    try {
-      axios
-        .post("http://localhost:4000/api/user/get", {
-          email: this.state.email,
-          password: this.state.password
-        })
-        .then(res => res.request.response)
-        .then(res => {
-          console.log(res);
-          let validate = JSON.parse(res).success;
-          this.validateLogin(validate);
-          this.context.isLoggedIn = this.state.loggedIn;
-          this.context.user.name = JSON.parse(res).name;
-          this.context.user.email = this.state.email;
-          if (this.state.loggedIn) {
-            this.setState({msg: ""});
-            this.props.history.push("/");
-            localStorage.setItem("isLoggedIn", this.state.loggedIn);
-            localStorage.setItem("userName", this.context.user.name);
-            localStorage.setItem("userEmail", this.context.user.email);
-          }else{
-            this.setState({msg: JSON.parse(res).message});
-          
-          }
-
-        });
-    } catch (error) {
+    handleSubmit = async event => {
+        console.log(this.context);
+        try {
+            axios
+                .post("http://localhost:4000/api/user/get", {
+                    email: this.state.email,
+                    password: this.state.password
+                })
+                .then(res => res.request.response)
+                .then(res => {
+                    console.log(res);
+                    let validate = JSON.parse(res).success;
+                    this.validateLogin(validate);
+                    this.context.isLoggedIn = this.state.loggedIn;
+                    this.context.user.name = JSON.parse(res).name;
+                    this.context.user.email = this.state.email;
+                    if (this.state.loggedIn) {
+                        this.setState({ msg: "" });
+                        this.props.history.push("/");
+                        localStorage.setItem("isLoggedIn", this.state.loggedIn);
+                        localStorage.setItem(
+                            "userName",
+                            this.context.user.name
+                        );
+                        localStorage.setItem(
+                            "userEmail",
+                            this.context.user.email
+                        );
+                    } else {
+                        this.setState({ msg: JSON.parse(res).message });
+                    }
+                });
+        } catch (error) {
             console.log(error);
         }
 
         event.preventDefault();
     };
 
-  render() {
-    return (
-      <div className="Login">
-      <p id="errorMsg">{this.state.msg}</p>
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="email" bSize="large ">
-            <FormLabel>Email</FormLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bSize="large">
-            <FormLabel>Password</FormLabel>
-            <FormControl
-              type="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
+    render() {
+        return (
+            <div className="Login">
+                <p id="errorMsg">{this.state.msg}</p>
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="email" bSize="large ">
+                        <FormLabel>
+                            <p>Email</p>
+                        </FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" bSize="large">
+                        <FormLabel>
+                            <span>Password</span>
+                        </FormLabel>
+                        <FormControl
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
 
                     <Button block disabled={!this.validateForm()} type="submit">
-                        Login
+                        <span>Login</span>
                     </Button>
                     <Link to="/register" className="nav-link">
-                        Sign Up
+                        <span>Sign Up</span>
                     </Link>
                 </form>
             </div>
