aeeabcc3ce4eb7c029853d0238765720d3219fed
Tuubaas <tobli@student.chalmers.se>
2019-03-12 13:46:13 +0100

    Small fix on create recipe

--- recipe-app/src/component/create-recipe.component.js
+++ recipe-app/src/component/create-recipe.component.js
@@ -8,150 +8,151 @@ import { Link } from "react-router-dom";
 import "../styles/CreateRecipe.css";
 
 export default class CreateRecipe extends Component {
-  constructor(props) {
-    super(props);
-    /*Preliminary strings, change to lists if possible, or parse them*/
-    this.state = {
-      title: "",
-      ingredients: "",
-      instructions: "",
-      categories: {}
+    constructor(props) {
+        super(props);
+        /*Preliminary strings, change to lists if possible, or parse them*/
+        this.state = {
+            title: "",
+            ingredients: "",
+            instructions: "",
+            categories: {}
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    handleOptions = selectedOptions => {
+        this.setState({
+            categories: selectedOptions
+        });
+    };
 
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  };
+    validateForm() {
+        return (
+            this.state.title.length > 0 &&
+            this.state.ingredients.length > 0 &&
+            this.state.instructions.length > 0
+        );
+    }
 
-  handleOptions = selectedOptions => {
-    this.setState({
-      categories: selectedOptions
-    });
-  };
+    handleSubmit = event => {
+        axios
+            .post("http://localhost:4000/api/recipe/add", {
+                title: this.state.title,
+                ingredients: this.state.ingredients,
+                instructions: this.state.instructions,
+                category: this.state.categories
+            })
+            .then(res => res.request.response)
+            .then(res => {
+                if (this.context.isLoggedIn) {
+                    axios.post(
+                        "http://localhost:4000/api/user/addCreatedRecipe",
+                        {
+                            email: this.context.user.email,
+                            _id: JSON.parse(res).data._id
+                        }
+                    );
+                }
+            });
+        this.props.history.push("/");
+        console.log("Outside of axios");
 
-  validateForm() {
-    return (
-      this.state.title.length > 0 &&
-      this.state.ingredients.length > 0 &&
-      this.state.instructions.length > 0
-    );
-  }
+        event.preventDefault();
+    };
 
-  handleSubmit = event => {
-    axios
-      .post("http://localhost:4000/api/recipe/add", {
-        title: this.state.title,
-        ingredients: this.state.ingredients,
-        instructions: this.state.instructions,
-        category: this.state.categories
-      })
-      .then(res => res.request.response)
-      .then(res => {
-        if (this.context.isLoggedIn) {
-          axios.post("http://localhost:4000/api/user/addCreatedRecipe", {
-            email: this.context.user.email,
-            _id: JSON.parse(res).data._id
-          });
-        }
-      });
-    this.props.history.push("/");
-    console.log("Outside of axios");
+    getOptions() {
+        let options = [];
+        var i = 1;
+        this.props.categories.forEach(element => {
+            options.push({ label: element.category, value: i });
+            i++;
+        });
+        return options;
+    }
 
-    event.preventDefault();
-  };
+    render() {
+        const { selectedOptions } = this.state.categories;
+        return (
+            /*TODO: Make into a form so that we can setStates above*/
 
-  getOptions() {
-    let options = [];
-    var i = 1;
-    this.props.categories.forEach(element => {
-      options.push({ label: element.category, value: i });
-      i++;
-    });
-    return options;
-  }
-
-  render() {
-    const { selectedOptions } = this.state.categories;
-    return (
-      /*TODO: Make into a form so that we can setStates above*/
-
-      <div>
-        <h2>Create Recipe</h2>
-        <div className="recipeContainer">
-          <div className="row mt-5">
-            <div className="col-sm-12">
-              <form onSubmit={this.handleSubmit}>
-                <FormGroup bsize="large">
-                  <FormLabel column sm="2">
-                    Title
-                  </FormLabel>
-                  <FormControl
-                    className="test"
-                    id="title"
-                    type="text"
-                    value={this.state.title}
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <FormLabel column sm="2">
-                    Ingredients
-                  </FormLabel>
-                  <FormControl
-                    as="textarea"
-                    rows="3"
-                    className="test"
-                    id="ingredients"
-                    type="text"
-                    value={this.state.ingredients}
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <FormLabel column sm="2">
-                    Instructions
-                  </FormLabel>
-                  <FormControl
-                    as="textarea"
-                    rows="3"
-                    className="test"
-                    id="instructions"
-                    value={this.state.instructions}
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <FormLabel column sm="2">
-                    Category
-                  </FormLabel>
-                  <ReactMultiSelectCheckboxes
-                    value={selectedOptions}
-                    onChange={this.handleOptions}
-                    options={this.getOptions()}
-                  />
-                </FormGroup>
-                <Link to="/">
-                  <Button
-                    block
-                    bsize="large"
-                    disabled={!this.validateForm()}
-                    type="submit"
-                  >
-                    Submit
-                  </Button>
-                </Link>
-              </form>
+            <div>
+                <h2>Create Recipe</h2>
+                <div className="recipeContainer">
+                    <div className="row mt-5">
+                        <div className="col-sm-12">
+                            <form onSubmit={this.handleSubmit}>
+                                <FormGroup bsize="large">
+                                    <FormLabel column sm="2">
+                                        Title
+                                    </FormLabel>
+                                    <FormControl
+                                        className="test"
+                                        id="title"
+                                        type="text"
+                                        value={this.state.title}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <FormLabel column sm="2">
+                                        Ingredients
+                                    </FormLabel>
+                                    <FormControl
+                                        as="textarea"
+                                        rows="3"
+                                        className="test"
+                                        id="ingredients"
+                                        type="text"
+                                        value={this.state.ingredients}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <FormLabel column sm="2">
+                                        Instructions
+                                    </FormLabel>
+                                    <FormControl
+                                        as="textarea"
+                                        rows="3"
+                                        className="test"
+                                        id="instructions"
+                                        value={this.state.instructions}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <FormLabel column sm="2">
+                                        Category
+                                    </FormLabel>
+                                    <ReactMultiSelectCheckboxes
+                                        value={selectedOptions}
+                                        onChange={this.handleOptions}
+                                        options={this.getOptions()}
+                                    />
+                                </FormGroup>
+                                <Button
+                                    block
+                                    bsize="large"
+                                    disabled={!this.validateForm()}
+                                    type="submit"
+                                >
+                                    Submit
+                                </Button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 CreateRecipe.contextType = AppContext;
